dae2a74147e496b3d93f5156b6c2599b347a829b
nick261505-svg <nick261505@gmail.com>
2026-09-18 16:01:11 +0900

    Configure Kakao Maps JavaScript key

--- map-config.js
+++ map-config.js
@@ -1,5 +1,5 @@
 // 카카오디벨로퍼스에서 발급한 JavaScript 키를 아래 따옴표 안에 넣으세요.
 // 앱 설정의 Web 플랫폼에 https://lottogood.github.io 도 함께 등록해야 합니다.
 window.LOTTOGOOD_MAP = {
-  kakaoJavascriptKey: ''
+  kakaoJavascriptKey: '500145831a8f8c274720c76c87a4903a'
 };
